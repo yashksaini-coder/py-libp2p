62ec49e72fb1546dc5583fe7501d4d0d40c860d3
yashksaini-coder <yashksaini89@gmail.com>
2025-11-24 23:29:56 +0530

    refactor: Move RateLimitError class to exceptions module for better organization and maintainability

--- libp2p/network/rate_limiter.py
+++ libp2p/network/rate_limiter.py
@@ -12,31 +12,9 @@ import logging
 import time
 from typing import Any
 
+from libp2p.network.exceptions import RateLimitError
+
 logger = logging.getLogger("libp2p.network.rate_limiter")
-
-
-class RateLimitError(Exception):
-    """Raised when rate limit is exceeded."""
-
-    def __init__(
-        self, message: str, consumed_points: int, remaining_points: int = 0
-    ) -> None:
-        """
-        Initialize rate limit error.
-
-        Parameters
-        ----------
-        message : str
-            Error message
-        consumed_points : int
-            Number of points consumed
-        remaining_points : int
-            Number of points remaining
-
-        """
-        super().__init__(message)
-        self.consumed_points = consumed_points
-        self.remaining_points = remaining_points
 
 
 class RateLimiter:
